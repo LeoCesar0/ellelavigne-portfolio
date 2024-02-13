e8d9ee409db076c0b8914342f5c1d208f058dce3
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-02-13 19:33:01 -0300

    feat: footer mobile

--- src/components/CardProject/index.jsx
+++ src/components/CardProject/index.jsx
@@ -8,57 +8,59 @@ export const CardProject = ({
   buttonColor = "#F4B7C4",
   figmalink,
   githubLink,
-  cardImg
+  cardImg,
 }) => {
   const buttonStyle = { backgroundColor: buttonColor, color: buttonTextColor }
   return (
     <>
       <div className={styles.card}>
-        <div className={styles.textCard}>
-          <h3 className={styles.titleCard}>{title}</h3>
-          <p className={styles.description}>{description}</p>
+        <h3 className={styles.titleCard}>{title}</h3>
+        <div className={styles.cardContainer}>
+          <div className={styles.content}>
+            <div className={styles.textCard}>
+              <p className={styles.description}>{description}</p>
+              <div className={styles.tools}>
+                <h3 className={styles.toolsTitle}>Tools</h3>
+                {tools.map((tool) => {
+                  return (
+                    <>
+                      <img
+                        key={tool}
+                        className={styles.icon}
+                        src={`${tool}.svg`}
+                        alt=""
+                      />
+                    </>
+                  )
+                })}
+              </div>
+              <div className={styles.buttons}>
+                {figmalink && (
+                  <a
+                    style={buttonStyle}
+                    className={styles.bntCard}
+                    target="blank"
+                    href={figmalink}
+                  >
+                    Figma
+                  </a>
+                )}
 
-          <div className={styles.tools}>
-            <h3 className={styles.toolsTitle}>Tools</h3>
-            {tools.map((tool) => {
-              return (
-                <>
-                  <img
-                    key={tool}
-                    className={styles.icon}
-                    src={`${tool}.svg`}
-                    alt=""
-                  />
-                </>
-              )
-            })}
+                {githubLink && (
+                  <a
+                    style={buttonStyle}
+                    className={styles.bntCard}
+                    target="blank"
+                    href={githubLink}
+                  >
+                    Github
+                  </a>
+                )}
+              </div>
+            </div>
           </div>
-          <div className={styles.buttons}>
-            {figmalink && (
-              <a
-                style={buttonStyle}
-                className={styles.bntCard}
-                target="blank"
-                href={figmalink}
-              >
-                Figma
-              </a>
-            )}
-
-            {githubLink && (
-              <a
-                style={buttonStyle}
-                className={styles.bntCard}
-                target="blank"
-                href={githubLink}
-              >
-                Github
-              </a>
-            )}
-          </div>
+          <img className={styles.cardImg} src={cardImg} alt="" />
         </div>
-
-        <img className={styles.cardImg} src={cardImg} alt="" />
       </div>
     </>
   )
