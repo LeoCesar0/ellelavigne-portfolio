370833597856570be76d6ab92d99d5de27a28c45
Leonardo César <leo_cesa@hotmail.com>
2024-03-17 13:04:54 -0300

    refactor: variables name

--- src/section/Projects/index.jsx
+++ src/section/Projects/index.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import { CardProject } from "../../components/CardProject";
 import { PROJECTS } from "../../static/projects";
 import styles from "./styles.module.scss";
+import { useMemo } from "react";
 
 export const Projects = () => {
+  const [projectOnView, setProjectsOnView] = useState(2);
+
+  const seeMore = () => {
+    setProjectsOnView(prev => prev + 3);
+  }
+
+  const projectsList = useMemo(() => {
+    return PROJECTS.slice(0, projectOnView);
+  }, [PROJECTS, projectOnView])
+
   return (
     <>
       <main className={styles.main} id="projetos">
         <div className="container">
           <h2 className={styles.title}>Projetos</h2>
           <div className={styles.cards}>
-            {PROJECTS.map((project, index) => {
+            {projectsList.map((project, index) => {
               return (
                 <CardProject
                   key={project.title + "-" + index}
@@ -17,6 +29,11 @@ export const Projects = () => {
                 />
               );
             })}
+            {projectsList.length < PROJECTS.length && (
+              <button className={styles.seeMore} onClick={seeMore}>
+                Ver mais
+              </button>
+            )}
           </div>
         </div>
       </main>
